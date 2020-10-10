0c701c9e5aa08cb12c09eccfb60a45fa2187c6c8
LeCongThuong <thuongle276@gmail.com>
2020-10-10 21:37:44 +0700

    fix loss func

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -65,7 +65,7 @@ class Pix2PixModel(BaseModel):
             # define loss functions
             self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
             self.criterionL1 = torch.nn.NLLLoss()
-            self.classifier_loss = torch.nn.BCEWithLogitsLoss()
+            self.classifier_loss = torch.nn.NLLLoss()
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
             self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
