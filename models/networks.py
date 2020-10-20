fix average pooling

models/networks.py
@@ -579,8 +579,8 @@ class NLayerDiscriminator(nn.Module):
         ]
         self.backbone = nn.Sequential(*sequence)
         gan_head_list = [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
-        num_features_list = [25088, 2048, num_classes]
-        classifier_head_list = [nn.Flatten()]
+        num_features_list = [ndf * nf_mult, 2048, num_classes]
+        classifier_head_list = [nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten()]
         classifier_head_list += [nn.Linear(num_features_list[i], num_features_list[i + 1]) for i in range(2)]
         self.classifier_head = nn.Sequential(*classifier_head_list)
         self.gan_head = nn.Sequential(*gan_head_list)